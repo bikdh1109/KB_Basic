696678c1c142b6e8b1812d05ca16ea883885ca90
bikdh1109 <sking1109s@gmail.com>
2025-03-28 08:52:52 +0900

    3월 27일자 기본 실습 문제 업로드

---
+++ 02_Vue/chapter06/test-vue-app/src/components/CheckboxItem4.vue
@@ -0,0 +1,21 @@
+<script setup>
+defineProps({
+  idol: {
+    type: Object,
+    required: true,
+    validator: (value) => {
+      return (
+        (typeof value.id === 'number' || typeof value.id === 'string') &&
+        typeof value.name === 'string' &&
+        typeof value.checked === 'boolean'
+      );
+    },
+  },
+});
+</script>
+
+<template>
+  <li><input type="checkbox" :checked="idol.checked" /> {{ idol.name }}</li>
+</template>
+
+<style scoped></style>
---
+++ 02_Vue/chapter06/test-vue-app/src/main.js
@@ -0,0 +1,14 @@
+import './assets/main.css';
+
+import { createApp } from 'vue';
+// import App from './App.vue';
+// import App from './App2.vue';
+// import App from './App3.vue';
+// import App from './App4.vue';
+import App from './App5.vue';
+
+// 전역 컴포넌트 설정
+// import CheckboxItem from './components/CheckboxItem.vue';
+// createApp(App).component('CheckboxItem', CheckboxItem).mount('#app');
+
+createApp(App).mount('#app');
